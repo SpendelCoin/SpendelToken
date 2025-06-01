cf3705a7a108f6b7cb32fe01b1428a6d5b489221
SpendelCoin <spendelcoin@gmail.com>
2025-06-01 16:27:37 -0500

    Create SpendelToken.sol

---
+++ contracts/SpendelToken.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract SpendelToken is ERC20, Ownable {
+    uint256 public maxWalletPercent = 1; // 1%
+    uint256 public launchTime;
+    bool public tradingOpen = false;
+
+    mapping(address => bool) private _isExcludedFromLimit;
+
+    constructor() ERC20("Spendel", "SPNDL") Ownable(msg.sender) {
+        _mint(msg.sender, 8888888888 * 10 ** decimals());
+        _isExcludedFromLimit[msg.sender] = true;
+    }
+
+    function openTrading() external onlyOwner {
+        tradingOpen = true;
+        launchTime = block.timestamp;
+    }
+
+    function setMaxWalletPercent(uint256 percent) external onlyOwner {
+        require(percent >= 1, "Too small");
+        maxWalletPercent = percent;
+    }
+
+    function excludeFromLimit(address account, bool excluded) external onlyOwner {
+        _isExcludedFromLimit[account] = excluded;
+    }
+
+    function _transfer(address from, address to, uint256 value) internal override {
+        super._transfer(from, to, value);
+
+        if (from == address(0) || _isExcludedFromLimit[to]) return;
+
+        if (tradingOpen && block.timestamp < launchTime + 1 hours) {
+            uint256 maxTokens = (totalSupply() * maxWalletPercent) / 100;
+            require(balanceOf(to) + value <= maxTokens, "Exceeds max wallet limit");
+        }
+    }
+}
